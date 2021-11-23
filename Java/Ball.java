d65d289599a3affbca11ce96927acdd26bed2d02
GoatersBro <89107011+GoatersBro@users.noreply.github.com>
2021-11-22 19:42:26 -0700

    Added: Ball Friction
    The ball can now slowdown

--- Java/Ball.java
+++ Java/Ball.java
@@ -2,11 +2,13 @@ package Java;
 //This is the ball and all of its moving stuff
 import java.awt.*;
 
-public class Ball {
+public class Ball implements Runnable{
     int GAME_WIDTH,GAME_HEIGHT;
     int x,y, xVelocity, yVelocity;
-    double loss = .05;
+    double loss = 2;
     final int BALL_SIZE = 20;
+
+    Thread ballThread;
 
     Ball(int width, int height){
         this.GAME_WIDTH = width;
@@ -15,12 +17,27 @@ public class Ball {
         x = (GAME_WIDTH/2)-(BALL_SIZE/2);
         y = (GAME_HEIGHT/2)-(BALL_SIZE/2);
         
-        xVelocity = 10;
-        yVelocity = -10;
+        ballThread = new Thread(this);
+        ballThread.start();
+
+        //These lines of code is for testing remove once tested!!!!!!!!!!!
+        xVelocity = -10;
+        yVelocity = 10;
     }
 
-    public void move(){//Setting the location of the ball
-        
+    public void run(){
+        while(true){
+            try {
+                Thread.sleep(250);
+            } catch (Exception e) {
+                System.out.println("Ball Fail stuff so run away and cry yourself to bed");
+            }
+
+            slowDown();
+        }
+    }
+
+    public void slowDown(){
         if(xVelocity>1){
             xVelocity-=loss;
         }else if(xVelocity<=1 && xVelocity >=-1){
@@ -36,9 +53,9 @@ public class Ball {
         }else{
             yVelocity += loss;
         }
-        
-        
-        
+    }
+
+    public void move(){//Setting the location of the ball
         //Setting the x and y
         x += xVelocity;
         y +=yVelocity;
